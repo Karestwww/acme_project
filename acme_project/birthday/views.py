7ec9fcb48ef2f49964d14135d00d498e7890d2c7
Karestwww <Karestwww@yandex.ru>
2024-05-08 14:28:32 +0300

    add users

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,30 +2,46 @@ from django.views.generic import (
     ListView, CreateView, UpdateView, DeleteView, DetailView
 )
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user 
+    
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
+
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
-class BirthdayUpdateView(UpdateView):
-    model = Birthday
-    form_class = BirthdayForm
-
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayDetailView(DetailView):
+class BirthdayDetailView(OnlyAuthorMixin, DetailView):
     model = Birthday 
 
     def get_context_data(self, **kwargs):
